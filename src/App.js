b0148db83b8e0f3d3abd955aa57a854a36728fd0
krish <krish1988@live.com>
2019-10-07 19:20:31 +0200

    broken our homepage into smaller components, fineshed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';  
 
 function App() {
   return  <div>
@@ -9,5 +9,5 @@ function App() {
     </div>;
 
 }
-
+ 
 export default App;
